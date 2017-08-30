cc8d9be065e534f5495420f8143e8e4850b53d85
Ηλιάδης Ηλίας <Xoristzatziki@users.noreply.github.com>
2017-08-30 18:54:18 +0300

    Create __pkginfo__.py

---
+++ _lib/OCP/__pkginfo__.py
@@ -0,0 +1,45 @@
+# --
+# copyright Ηλιάδης Ηλίας 2017.
+# contact http://gnu.kekbay.gr/OCPcompanion/  -- mailto:OCPcompanion@kekbay.gr
+#
+# This file is part of OCPcompanion.
+#
+# OCPcompanion is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation, either version 3.0 of the License, or (at your
+# option) any later version.
+#
+# OCPcompanion is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+# for more details.
+#
+# You should have received a copy of the GPLv3 along
+# with OCPcompanion. If not, see <http://www.gnu.org/licenses/>.
+
+modname = 'OCPcompanion'
+distname = 'OCPcompanion'
+
+numversion = (0, 0, 10)
+version = '.'.join(map(str, numversion))
+
+license = 'GPLv3'
+copyright = '''Copyright (c) Ηλιάδης Ηλίας 2014 (Αθήνα, Ελλάδα).
+mailto:OCPcompanion@kekbay.gr'''
+
+short_desc = "Basic Functions and Windows to create a GUI application"
+
+long_desc = """Usefull graphical functions and constants
+that are used to create graphical interfaces.
+"""
+
+author = "Ηλιάδης Ηλίας"
+author_email = "OCPcompanion@kekbay.gr"
+
+web = "http://gnu.kekbay.gr/OCPvblike/projects/%s" % modname
+ftp = "http://ftp.kekbay.gr/OCPvblike/pub/%s/" % modname
+#mailinglist = ""
+
+
+#debian_name = 'OCPcompanion'
+#debian_maintainer_email = ''
